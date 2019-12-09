34b7f86f28e9cb69249cddf968dad2c5e31fe1af
Owen Morgan <owen.morgan95@gmail.com>
2019-12-08 20:02:17 -0500

    added openbox to wal_set.sh

--- scripts/scripts/set_v4l2.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-v4l2-ctl -c white_balance_automatic=0 
-v4l2-ctl -c brightness=128
--- scripts/scripts/wal_set.sh
+++ scripts/scripts/wal_set.sh
@@ -16,11 +16,25 @@ reload_dunst() {
         -frame_color "${color7:-#000000}" &
 }
 
-reload_mpv() {
-  sed -i "/sub-color=/c\sub-color=\"${foreground:-#FFFFFF}\"" ~/.config/mpv/mpv.conf
-  sed -i "/sub-border-color=/c\sub-border-color=\"${background:-#FFFFFF}\"" ~/.config/mpv/mpv.conf 
-  sed -i "/sub-shadow-color=/c\sub-shadow-color=\"${background:-#FFFFFF}\"" ~/.config/mpv/mpv.conf 
-}
+
+reload_openbox() {
+  sed -i "/menu.items.bg.color:/c\menu.items.bg.color:\ ${background:-#FFFFFF}" ~/.themes/active/openbox-3/themerc
+  sed -i "/menu.items.text.color:/c\menu.items.text.color:\ ${foreground:-#eeeeee}" ~/.themes/active/openbox-3/themerc
+  sed -i "/menu.title.bg.color:/c\menu.title.bg.color:\ ${background:-#FFFFFF}" ~/.themes/active/openbox-3/themerc
+  sed -i "/menu.border.color:/c\menu.border.color:\ ${foreground:-#FFFFFF}" ~/.themes/active/openbox-3/themerc
+  sed -i "/window.active.title.bg.color:/c\window.active.title.bg.color:\ ${foreground:-#FFFFFF}" ~/.themes/active/openbox-3/themerc
+  sed -i "/window.inactive.title.bg.color:/c\window.inactive.title.bg.color:\ ${background:-#FFFFFF}" ~/.themes/active/openbox-3/themerc
+  sed -i "/window.active.label.text.color:/c\window.active.label.text.color:\ ${background:-#FFFFFF}" ~/.themes/active/openbox-3/themerc
+  sed -i "/window.inactive.label.text.color:/c\window.inactive.label.text.color:\ ${foreground:-#EEEEEE}" ~/.themes/active/openbox-3/themerc
+  sed -i "/window.active.border.color:/c\window.active.border.color:\ ${background:-#FFFFFF}" ~/.themes/active/openbox-3/themerc
+  sed -i "/window.inactive.border.color:/c\window.inactive.border.color:\ ${background:-#FFFFFF}" ~/.themes/active/openbox-3/themerc
+  sed -i "/window.active.client.color:/c\window.active.client.color:\ ${background:-#FFFFFF}" ~/.themes/active/openbox-3/themerc
+  sed -i "/window.inactive.client.color:/c\window.inactive.client.color:\ ${background:-#FFFFFF}" ~/.themes/active/openbox-3/themerc
+  sed -i "/window.active.handle.bg.color:/c\window.active.handle.bg.color:\ ${background:-#FFFFFF}" ~/.themes/active/openbox-3/themerc
+  sed -i "/window.inactive.handle.bg.color:/c\window.inactive.handle.bg.color:\ ${background:-#FFFFFF}" ~/.themes/active/openbox-3/themerc
+  sed -i "/window.active.grip.bg.color:/c\window.active.grip.bg.color:\ ${background:=#FFFFFF}" ~/.themes/active/openbox-3/themerc
+  sed -i "/window.inactive.grip.bg.color/c\window.inactive.grip.bg.color:\ ${background:-#FFFFFF}" ~/.themes/active/openbox-3/themerc
+ }
 
 reload_cava(){
   sed -i "/gradient_color_1=/c\gradient_color_1=\"${color3:-#000000}\"" ~/.config/cava/config
@@ -47,8 +61,8 @@ reload_zathura() {
   
 
 main() {
+    reload_openbox
     reload_dunst
-    reload_mpv
     reload_zathura
     reload_cava
 }
