71e1c8b0bd7964603c0d3acfb3133202c48e3b74
Owen Morgan <owen.morgan95@gmail.com>
2019-12-12 06:00:03 -0500

    restyle volume and brightness notifications for light theme

--- scripts/scripts/vol_not.sh
+++ scripts/scripts/vol_not.sh
@@ -17,7 +17,7 @@ function send_notification {
     volume=`get_volume`
     # Make the bar with the special character ─ (it's not dash -)
     # https://en.wikipedia.org/wiki/Box-drawing_character
-    bar=$(seq -s "─" $((($volume / 5)+1)) | sed 's/[0-9]//g')
+    bar=$(seq -s "━" $((($volume / 5)+1)) | sed 's/[0-9]//g')
     # Send the notification
     # use notify-send once stable dunst supports stack_tags
     notify-send --hint=string:x-dunst-stack-tag:volume "$volume      $bar" -i $icon
@@ -26,19 +26,19 @@ function send_notification {
 case $1 in
     up)
 	if is_mute ; then
-      icon='/home/om/.icons/material-design-icons/av/drawable-hdpi/ic_volume_off_white_24dp.png'
+      icon='/home/om/.icons/material-design-icons/av/drawable-hdpi/ic_volume_off_black_24dp.png'
       send_notification
 	else
-      icon='/home/om/.icons/material-design-icons/av/drawable-hdpi/ic_volume_up_white_24dp.png'
+      icon='/home/om/.icons/material-design-icons/av/drawable-hdpi/ic_volume_up_black_24dp.png'
 	    send_notification
 	fi
 	;;
     down)
 	if is_mute ; then
-      icon='/home/om/.icons/material-design-icons/av/drawable-hdpi/ic_volume_off_white_24dp.png'
+      icon='/home/om/.icons/material-design-icons/av/drawable-hdpi/ic_volume_off_black_24dp.png'
       send_notification
 	else
-      icon='/home/om/.icons/material-design-icons/av/drawable-hdpi/ic_volume_down_white_24dp.png'
+      icon='/home/om/.icons/material-design-icons/av/drawable-hdpi/ic_volume_down_black_24dp.png'
 	    send_notification
 	fi
 	;;
@@ -46,10 +46,10 @@ case $1 in
     	# Toggle mute
   echo "mute!!!"
 	if is_mute ; then
-      icon='/home/om/.icons/material-design-icons/av/drawable-hdpi/ic_volume_off_white_24dp.png'
+      icon='/home/om/.icons/material-design-icons/av/drawable-hdpi/ic_volume_off_black_24dp.png'
       send_notification
 	else
-      icon='/home/om/.icons/material-design-icons/av/drawable-hdpi/ic_volume_up_white_24dp.png'
+      icon='/home/om/.icons/material-design-icons/av/drawable-hdpi/ic_volume_up_black_24dp.png'
 	    send_notification
 	fi
 	;;
