cad9b7ebdf52115e0f05f8ce426aa1b478299ece
Owen Morgan <owen.morgan95@gmail.com>
2019-12-17 17:15:24 -0500

    updated battery notifications, clean_homedir.service

--- systemd/.config/systemd/user/bat.sh
+++ systemd/.config/systemd/user/bat.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env bash
 
-/usr/bin/notify-send "hello"
 
 BATTINFO=`acpi -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:30:00 && `echo $BATTINFO | cut -f 5 -d " "` > 00:25:01 ]]; then 
-    DISPLAY=:0 /usr/bin/notify-send "Battery low: 30 minutes left" -i "battery-empty-symbolic" -u normal
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 01:30:00 && `echo $BATTINFO | cut -f 5 -d " "` > 00:25:01 ]]; then 
+    DISPLAY=:0 /usr/bin/notify-send "Battery low: 30 minutes left" -i "/home/om/.icons/material-design-icons/device/drawable-mdpi/ic_battery_alert_black_48dp.png" -u normal
 fi
 
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:20:00 && `echo $BATTINFO | cut -f 5 -d " "` > 00:15:01 ]]; then 
-    DISPLAY=:0 /usr/bin/notify-send "Battery low:  20 minutes left" -i "battery-empty-symbolic" -u normal
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:24:00 && `echo $BATTINFO | cut -f 5 -d " "` > 00:15:01 ]]; then 
+    DISPLAY=:0 /usr/bin/notify-send "Battery low:  20 minutes left" -i "/home/om/.icons/material-design-icons/device/drawable-mdpi/ic_battery_alert_black_48dp.png" -u normal
 fi
 
 if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 && `echo $BATTINFO | cut -f 5 -d " "` > 00:10:01 ]]; then 
-    DISPLAY=:0 /usr/bin/notify-send "Battery low:  15 minutes left" -i "battery-empty-symbolic" -u normal
+    DISPLAY=:0 /usr/bin/notify-send "Battery low:  15 minutes left" -i "/home/om/.icons/material-design-icons/device/drawable-mdpi/ic_battery_alert_black_48dp.png" -u normal
 fi
 
 if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:10:00 && `echo $BATTINFO | cut -f 5 -d " "` > 00:05:01 ]]; then 
-    DISPLAY=:0 /usr/bin/notify-send "Battery critical:  10 minutes left" -i "battery-empty-symbolic" -u critical
+    DISPLAY=:0 /usr/bin/notify-send "Battery critical:  10 minutes left" -i "/home/om/.icons/material-design-icons/device/drawable-mdpi/ic_battery_alert_black_48dp.png" -u critical
 fi
 
 if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:05:00 ]]; then 
-    DISPLAY=:0 /usr/bin/notify-send "Battery critical:  5 minutes left" -i "battery-empty-symbolic" -u critical
+    DISPLAY=:0 /usr/bin/notify-send "Battery critical:  5 minutes left" -i "/home/om/.icons/material-design-icons/device/drawable-mdpi/ic_battery_alert_black_48dp.png" -u critical
 fi
