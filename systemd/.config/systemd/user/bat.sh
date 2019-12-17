53ba106c067e037ed019a0e6e990b1b9dfb8477f
Owen Morgan <owen.morgan95@gmail.com>
2019-12-17 16:14:43 -0500

    moving systemd scripts to .config/systemd/user

--- systemd/.config/systemd/user/bat.sh
+++ systemd/.config/systemd/user/bat.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
+/usr/bin/notify-send "hello"
+
 BATTINFO=`acpi -b`
 if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:30:00 && `echo $BATTINFO | cut -f 5 -d " "` > 00:25:01 ]]; then 
-    DISPLAY=:0 /usr/bin/notify-send "Battery low: 30 minutes to midnight" -i "battery-empty-symbolic" -u normal
+    DISPLAY=:0 /usr/bin/notify-send "Battery low: 30 minutes left" -i "battery-empty-symbolic" -u normal
 fi
 
 if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:20:00 && `echo $BATTINFO | cut -f 5 -d " "` > 00:15:01 ]]; then 
-    DISPLAY=:0 /usr/bin/notify-send "Battery low:  20 minutes to midnight" -i "battery-empty-symbolic" -u normal
+    DISPLAY=:0 /usr/bin/notify-send "Battery low:  20 minutes left" -i "battery-empty-symbolic" -u normal
 fi
 
 if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 && `echo $BATTINFO | cut -f 5 -d " "` > 00:10:01 ]]; then 
-    DISPLAY=:0 /usr/bin/notify-send "Battery low:  15 minutes to midnight" -i "battery-empty-symbolic" -u normal
+    DISPLAY=:0 /usr/bin/notify-send "Battery low:  15 minutes left" -i "battery-empty-symbolic" -u normal
 fi
 
 if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:10:00 && `echo $BATTINFO | cut -f 5 -d " "` > 00:05:01 ]]; then 
-    DISPLAY=:0 /usr/bin/notify-send "Battery critical:  10 minutes to midnight" -i "battery-empty-symbolic" -u critical
+    DISPLAY=:0 /usr/bin/notify-send "Battery critical:  10 minutes left" -i "battery-empty-symbolic" -u critical
 fi
 
 if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:05:00 ]]; then 
-    DISPLAY=:0 /usr/bin/notify-send "Battery critical:  5 minutes to midnight" -i "battery-empty-symbolic" -u critical
+    DISPLAY=:0 /usr/bin/notify-send "Battery critical:  5 minutes left" -i "battery-empty-symbolic" -u critical
 fi
