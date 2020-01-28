7db5d62779ffe5d1344dca006e204db065396d28
Owen Morgan <owen.morgan95@gmail.com>
2020-01-27 22:22:56 -0500

    tweaks polybar launch script to hopefully eliminate glitch that causes bar2 to not appear on every monitor. removes tray from polybar config.

--- scripts/scripts/polyset.sh
+++ scripts/scripts/polyset.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
-pkill polybar
+# terminate existing bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
   echo $m
