69450953ea1c0369706e8378aa321cec6c5404f7
Owen Morgan <owen.morgan95@gmail.com>
2020-02-05 09:48:32 -0500

    tweaks qutebrowser config

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -33,6 +33,7 @@ c.tabs.indicator.width = 0
 c.fonts.default_family = '"Source Code Pro"'
 c.fonts.default_size = '10pt'
 c.fonts.tabs = '12pt Avenir'
+c.fonts.contextmenu = '12pt Avenir'
 
 # downloads
 c.downloads.location.directory = '/home/om/temp/'
