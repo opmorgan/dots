94ffbfcf0c8f7b7be3b925bc9c085e019f5f82d1
Owen Morgan <owen.morgan95@gmail.com>
2020-04-26 17:19:43 -0400

    cleans up clean_homedir service

--- scripts/scripts/clean_homedir.sh
+++ scripts/scripts/clean_homedir.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-mv $HOME/Downloads/* $HOME/temp/
-mv $HOME/Desktop/* $HOME/temp/
-
-/usr/bin/trash-put $HOME/Downloads
-/usr/bin/trash-put $HOME/Desktop
+for FOLDER in $HOME/Downloads $HOME/Desktop
+do
+  if test -e "$FOLDER"; then
+    # check if it has any contents
+    if [ "$(ls -A "$FOLDER")" ]; then
+      # if it does, move them, then delete folder
+      mv $FOLDER/* $HOME/temp/
+      /usr/bin/trash-put "$FOLDER"
+    else 
+      # if it doesn't, just delete folder
+      /usr/bin/trash-put "$FOLDER"
+    fi
+  fi
+done
