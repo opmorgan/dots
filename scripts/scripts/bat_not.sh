875a3eea2879d91dfaa3c90f5d722ddefdeba38c
Owen Morgan <owen.morgan95@gmail.com>
2020-07-23 17:32:06 -0400

    updates tmux keybinds

--- scripts/scripts/bat_not.sh
+++ scripts/scripts/bat_not.sh
@@ -2,16 +2,16 @@
 
 # This script will generate a notification showing low battery charge as reported by acpi. Run it at some interval with a systemd service to show a notification when your battery runs low.
 
-icon_30="$HOME/scripts/resources/bat_not/ic_battery_30_black_48dp.png"
-icon_20="$HOME/scripts/resources/bat_not/ic_battery_20_black_48dp.png"
-icon_critical="$HOME/scripts/resources/bat_not/ic_battery_alert_black_48dp.png"
+icon_30="$HOME/scripts/resources/bat_not/ic_battery_30_white_48dp.png"
+icon_20="$HOME/scripts/resources/bat_not/ic_battery_20_white_48dp.png"
+icon_critical="$HOME/scripts/resources/bat_not/ic_battery_alert_white_48dp.png"
 
 bat_pct_str=$(acpi -b | cut -f 4 -d " ")
 bat_pct=${bat_pct_str:0:-2}
 discharging=$(acpi -b | grep Discharging)
 
 if [[ "$discharging" && \
-  "$bat_pct" -lt 30 && \
+  "$bat_pct" -lt 100 && \
   "$bat_pct" -gt 20 ]]; then
   notify-send "Battery low: $bat_pct% left" -i $icon_30 -u normal
 fi
