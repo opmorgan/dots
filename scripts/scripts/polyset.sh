eebb3a3de4ca725e81fb75c74ac0fbc38b6d023c
Owen Morgan <owen.morgan95@gmail.com>
2020-07-24 13:48:29 -0400

    updates polybar

--- scripts/scripts/polyset.sh
+++ scripts/scripts/polyset.sh
@@ -6,11 +6,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 # while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# for m in $(polybar --list-monitors | cut -d":" -f1); do
-#   echo $m
-#   MONITOR=$m polybar --reload bar1
-# done
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+  echo $m
+  MONITOR=$m polybar --reload bar1
+done
 
-echo "---" | tee -a /tmp/polybar1.log
-polybar bar1 >>/tmp/polybar1.log 2>&1 &
-echo "Bars launched..."
+# echo "---" | tee -a /tmp/polybar1.log
+# polybar bar1 >>/tmp/polybar1.log 2>&1 &
+# echo "Bars launched..."
