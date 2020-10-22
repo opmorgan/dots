ed00f7e35ba75329896fdd769b65253569412dad
Owen Morgan <owen.morgan95@gmail.com>
2020-10-22 16:45:21 -0400

    Tweak polybar launch script (polyset) to only launch on one monitor. Tweak dinit to use wal's wallpaper.

--- scripts/scripts/polyset.sh
+++ scripts/scripts/polyset.sh
@@ -6,8 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-  echo $m
-  MONITOR=$m polybar --reload bar1 &
+## launch on primary monitor only
+polybar --reload bar1 &
+polybar --reload bar2 &
+
+## open on all monitors
+# for m in $(polybar --list-monitors | cut -d":" -f1); do
+#   echo $m
+#   MONITOR=$m polybar --reload bar1 &
   # MONITOR=$m polybar --reload bar2 &
-done
+# done
