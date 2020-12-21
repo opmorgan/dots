f2ed95639f98a02d3d701c2aaa3e8cd6d2ba122d
Owen Morgan <owen.morgan95@gmail.com>
2020-12-21 14:08:29 -0500

    Add nvim templates

---
+++ nvim/.config/nvim/templates/common.R
@@ -0,0 +1,28 @@
+knitr::opts_chunk$set(
+  warning = F,
+  message = F,
+  comment = "#>",
+  collapse = TRUE,
+  cache = TRUE,
+  fig.retina = 0.8, # figures are either vectors or 300 dpi diagrams
+  dpi = 300,
+  out.width = "70%",
+  fig.align = "center",
+  fig.width = 6,
+  fig.asp = 0.618,
+  fig.show = "hold",
+  results = "asis"
+)
+
+options(
+  digits = 3,
+  str = strOptions(strict.width = "cut"),
+  knitr.kable.NA = ""
+)
+
+if (knitr::is_latex_output()) {
+  knitr::opts_chunk$set(width = 69)
+  options(width = 69)
+  options(crayon.enabled = FALSE)
+  options(cli.unicode = TRUE)
+}
