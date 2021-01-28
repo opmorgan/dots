4b5f4edb8046867fbc7d8dd063955f8bdd35d8f6
Owen Morgan <owen.morgan95@gmail.com>
2021-01-28 09:12:18 -0500

    Add emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,5 +1,3 @@
-#+NAME: init.el
-#+BEGIN_SRC elisp
 (require 'package)
 
 ;; Get the l33t package archive
@@ -21,7 +19,7 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-(window-divider-mode -1)
+;(window-divider-mode 1)
 (setq inhibit-splash-screen t)
 (setq inhibit-startup-message t)
 
@@ -42,4 +40,33 @@
 
 ;; OPTIONAL Live reloading of changes
 (global-auto-revert-mode 1)
-#+END_SRC
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(ewal-spacemacs-themes ewal evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Setup use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+;; ewal!
+(use-package ewal
+  :init (setq ewal-use-built-in-always-p nil
+              ewal-use-built-in-on-failure-p t
+              ewal-built-in-palette "sexy-material"))
+(use-package ewal-spacemacs-themes
+  :init (progn
+          (show-paren-mode +1))
+  :config (progn
+            (load-theme 'ewal-spacemacs-modern t)
+            (enable-theme 'ewal-spacemacs-modern)))
