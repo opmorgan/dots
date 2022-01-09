9dc9450d539aa2621699c02b4e235b63c47c92da
Owen Morgan <owen.morgan95@gmail.com>
2022-01-09 16:00:22 -0500

    Update xinitrc to launch st with tabbed

--- wal/.cache/wal/colors-wal-tabbed.h
+++ wal/.cache/wal/colors-wal-tabbed.h
@@ -1,4 +1,4 @@
-static const char* selbgcolor   = "#dc8cc3";
+static const char* selbgcolor   = "#6ca0a3";
 static const char* selfgcolor   = "#3f3f3f";
 static const char* normbgcolor  = "#3f3f3f";
 static const char* normfgcolor  = "#dcdccc";
--- wal/.config/wal/templates/colors-wal-tabbed.h
+++ wal/.config/wal/templates/colors-wal-tabbed.h
@@ -1,4 +1,4 @@
-static const char* selbgcolor   = "{color13}";
+static const char* selbgcolor   = "{color4}";
 static const char* selfgcolor   = "{background}";
 static const char* normbgcolor  = "{background}";
 static const char* normfgcolor  = "{foreground}";
