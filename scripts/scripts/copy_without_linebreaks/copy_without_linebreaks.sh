45ce5a6c80220f3d57ed58a285744f48fb3e2c32
Owen Morgan <owen.morgan95@gmail.com>
2022-01-15 18:12:00 -0500

    Tweak linebreak copying script to eliminate trailing whitespace

--- scripts/scripts/copy_without_linebreaks/copy_without_linebreaks.sh
+++ scripts/scripts/copy_without_linebreaks/copy_without_linebreaks.sh
@@ -7,7 +7,7 @@
 
 # secondary source: https://superuser.com/questions/796292/is-there-an-efficient-way-to-copy-text-from-a-pdf-without-the-line-breaks
 
-# Parses currently selected text and removes 
+# Parses currently selected text and removes
 # newlines
 
 while $HOME/scripts/copy_without_linebreaks/clipnotify;
@@ -16,11 +16,15 @@ do
   CopiedText="$(xsel -b)"
   if [[ $SelectedText != *"file:///"* ]]; then
     ModifiedTextPrimary="$(echo "$SelectedText" | tr -s '\n' ' ')"
-    echo -n "$ModifiedTextPrimary" | xsel -i
+    # Remove final trailing space
+    ModifiedTextPrimary_2="${ModifiedTextPrimary%?}"
+    echo -n "$ModifiedTextPrimary_2" | xsel -i
   fi
   if [[ $CopiedText != *"file:///"* ]]; then
-    ModifiedTextClipboard="$(echo "$CopiedText" | tr -s '\n' ' '  )"
-    echo -n "$ModifiedTextClipboard" | xsel -bi
+    ModifiedTextClipboard="$(echo "$CopiedText" | tr -s '\n' ' ')"
+    # Remove final trailing space
+    ModifiedTextClipboard_2="${ModifiedTextClipboard%?}"
+    echo -n "$ModifiedTextClipboard_2" | xsel -bi
   fi
 done
 
