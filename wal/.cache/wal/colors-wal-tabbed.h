ccf0909b1f8a1039076b01e4def36977790636ac
Owen Morgan <owen.morgan95@gmail.com>
2022-09-02 19:02:05 -0400

    Remove wal cache from version control; style rofi

--- wal/.cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#6ca0a3";
-static const char* selfgcolor   = "#3f3f3f";
-static const char* normbgcolor  = "#3f3f3f";
-static const char* normfgcolor  = "#dcdccc";
-static const char* urgbgcolor   = "#7f9f7f";
-static const char* urgfgcolor   = "#dcdccc";
