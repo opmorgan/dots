32051c2cb7200268b8cbb5e043c6ec2d8572d0ea
Owen Morgan <owen.morgan95@gmail.com>
2022-09-05 03:53:17 -0400

    Update outline for deploy_dots.sh

--- deploy_dots.sh
+++ deploy_dots.sh
@@ -19,6 +19,16 @@ stow_from_module () {
   echo "test"
 }
 
+## Another way (symlinks)
+## In each module, have a scipt that symlinks config files to appropriate paths, creating folders if needed (prompt user with a warning). The destination directory should draw from an env variable, $DOTS_TARGET_DIR.
+## A script, deploy.sh, at the top level, should enable you to:
+## deploy --modules base pons theme-joel (deploy all modeules from base, then pons)
+## deploy base bspwm (deplot dot package (stow lingo) bswpm from the module base, according to the script in base/bspwm/make-symlinks.sh
+## make_symlinks.sh should specify the appropriate symlink paths for each fdile int he directory, referring to the target directory with an env variable $DOTS_TARGET_DIR, and building the source dir from $DOTS_SOURCE_DIR (dots/) + arg1 (base) + arg 2 (bspwm).
+## show warnings and prompt for confirmation if:
+## a symlink needs to create a new folder
+## a symlink needs to overwrite an existing file.
+
 
 ## Initialize space-separated list of packages to stow
 PACKAGES_TO_STOW=""
