e35d4b16c2804956ef209463e614e640bccea42b
Owen Morgan <owen.morgan95@gmail.com>
2022-09-05 03:17:04 -0400

    Remove wal cache from version control

--- wal/.cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#dcdccc", "#3f3f3f" },
-	[SchemeSel] = { "#dcdccc", "#cc9393" },
-	[SchemeOut] = { "#dcdccc", "#93e0e3" },
-};
