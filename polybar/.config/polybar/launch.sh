d7d9fbcba16468c0ffc76237a3895a6d2aa3098d
Owen Morgan <owen.morgan95@gmail.com>
2022-09-08 18:10:53 -0400

    Tweak polybar config

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,8 +7,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 
-# Launch bar1 and bar2
-polybar bar1 &
-polybar bar2
+polybar bar1
 
-echo "Bars launched..."
+echo "Bar(s) launched..."
